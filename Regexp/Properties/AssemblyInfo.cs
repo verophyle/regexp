f5449cfbb0d7dc5684c70f654ace61e71df860fe
Gordon Tisher <gordon@balafon.net>
2017-01-13 16:21:36 -0800

    renaming files for multiple frameworks; encrypt signing key

--- Regexp/Properties/AssemblyInfo.cs
+++ Regexp/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Verophyle.Regexp")]
 [assembly: AssemblyDescription("Simple DFA-based regular expressions for .NET.")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("aa43d0f6-afc3-4ed4-844b-de47e11e36fd")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.*")]
 [assembly: AssemblyInformationalVersion("1.2.0")]
